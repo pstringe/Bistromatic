d2fa51f78c3d7fd4c7e02a90db9cb362e0d0d05f
Poitier Stringer <pstringe@e1z2r9p2.42.us.org>
2017-11-27 12:59:37 -0800

    ft_isalpha compiles and works for all unit tests, but something is wrong in the way I link in my make file. I'm going to try changing the order

--- mod/ft_isalpha/srcs/ft_isalpha_test.c
+++ mod/ft_isalpha/srcs/ft_isalpha_test.c
@@ -1,31 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   isalpha_test.c                                     :+:      :+:    :+:   */
+/*   ft_isalpha_test.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/27 11:34:25 by pstringe          #+#    #+#             */
-/*   Updated: 2017/11/27 11:53:31 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/11/27 12:57:16 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <ctype.h>
 
-int		ft_isalpha(int c);
+int		ft_isalpha();
 
 int		main(void)
 {
-	int i;
+	int	 i;
 	int ft;
 	int re;
 
-	i = 100;
-	while (++i <= 172)
+	i = 64;
+	while (++i <= 127)
 	{
-		re = isalpa(i);
-		ft = ft_isalpha(i);	
-		printf("input:\t%d\nis_apla:\t%d\n ft_isalpha\t%d\n\n", i, re, ft);
+		re = isalpha(i);
+		ft = ft_isalpha(i);
+		if(re != ft)
+		{	
+			printf("input:\t\t%d\nis_alpha:\t%d\nft_isalpha\t%d\n\n", i, re, ft);
+		}
 	}
+	printf("----end----");
 }
