38266d8d904f272ef771ce5081845671f88cf920
Poitier Stringer <pstringe@e1z2r9p23.42.us.org>
2017-11-29 15:09:39 -0800

    to lower works for all inputs representable by an unsigned char

---
+++ tests/tolower/ft_tolower_test.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tolower_test.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/29 14:17:25 by pstringe          #+#    #+#             */
+/*   Updated: 2017/11/29 15:06:39 by pstringe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	FT_TOLOWER_TEST_H
+# define FT_TOLOWER_TEST_H
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <xlocale.h>
+
+int		ft_tolower(int c);
+#endif
