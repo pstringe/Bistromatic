9652f41372dfb5e3fbee90f6014a43ba584bdaba
Poitier Stringer <pstringe@e1z2r9p23.42.us.org>
2017-11-29 18:41:59 -0800

    strncpy works for all tests

---
+++ tests/strncpy/ft_strncpy_test.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy_test.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/29 16:30:15 by pstringe          #+#    #+#             */
+/*   Updated: 2017/11/29 18:33:11 by pstringe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef		FT_STRNCPY_TEST_H
+# define	FT_STRNCPY_TEST_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char	*ft_strncpy(char *dst, const char *src, size_t len);
+#endif
