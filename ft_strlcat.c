fb5db7d6d0781f22865dea3b5bb1649d5b5752f4
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-08 13:41:57 -0800

    ft_strlcat works for all tests

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 14:21:38 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/05 17:21:43 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/08 13:38:56 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,18 @@
 
 int		ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	tmp;
 	size_t	len;
 	size_t	cpy;
+	size_t	lft;
 
-	tmp = ft_strlen(dst);
+	cpy = ft_strlen(dst);
 	len = ft_strlen(src);
-
-	if (size > 0 && tmp < tmp - 1)
+	lft = size - cpy - 1;
+	if (cpy < size)
 	{
-		if (len > size - tmp)
-		{
-			cpy = size - tmp - 1;
-		}
-		else
-		{
-			cpy = len;
-		}
-		ft_memcpy(dst + tmp, src, cpy);
-		dst[tmp+ cpy] = '\0';
+		ft_memcpy((dst + cpy), src, lft);
+		*(dst + (size - 1)) = '\0';
+		return (cpy + len);
 	}
-	return (tmp + len);
+	return (size + len);
 }
