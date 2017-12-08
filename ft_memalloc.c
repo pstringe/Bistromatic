479cd1b708b88bf86db7b14f4d1462ed3a8f623b
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-08 13:14:38 -0800

    ft_memalloc works for all tests

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -1,43 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*   ft_memalloc.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/12/05 19:22:58 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/08 13:12:26 by pstringe         ###   ########.fr       */
+/*   Created: 2017/12/08 13:06:31 by pstringe          #+#    #+#             */
+/*   Updated: 2017/12/08 13:09:59 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memmove(void *dst, const void *src, size_t len)
+void	*ft_memalloc(size)
 {
-	char *d;
-	const char *s;
+	void	*memory;
 
-	d = (char *)dst;
-	s = (const char *)src;
-
-	if (s < d)
+	memory = (void *)malloc(size);
+	if (memory == NULL)
 	{
-		s += len;
-		d += len;
-
-		while (len && src && dst)
-		{
-			*d--= *s--;
-			len--;
-		}
+		return(NULL);
 	}
-	else
-	{
-		while (len && src && dst)
-		{
-			*d++ = *s++;
-			len--;
-		}
-	}
-	return(dst);
+	ft_memset (memory, 0, size);
+	return(memory);
 }
