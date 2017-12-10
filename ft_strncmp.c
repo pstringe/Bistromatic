321d610f79e95f454bee116dc1823a9166a87d81
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-09 19:46:18 -0800

    strncmp works for all tests

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/04 19:36:40 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/04 19:52:17 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/09 19:44:12 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,21 @@
 
 int		ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned char uc1;
-	unsigned char uc2;
+	size_t i;
 
+	i = 0;
 	if (n == 0)
 	{
 		return (0);
 	}
 
-	while (n-- > 0 && *s1 == *s2)
+	while (*(s1 + i) == *(s2 + i) && *(s1 + i) && i < n)
 	{
-		if (n == 0 || *s1 == '\0')
-		{
-			return (0);
-		}
-		s1 ++;
-		s2 ++;
+		i++;
 	}
-
-	uc1 = (unsigned char)s1;
-	uc2 = (unsigned char)s2;
-	if (uc1 < uc2)
+	if (i == n)
 	{
-		return (1);
+		return ((unsigned char)*(s1 + i - 1) - (unsigned char)*(s2 + i - 1));
 	}
-	else if (uc1 > uc2)
-	{
-		return (-1);
-	}
-	else
-	{
-		return (0);
-	}
+	return ((unsigned char)*(s1 + i) - (unsigned char)*(s2 + i));
 }
