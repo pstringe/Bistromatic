bf75d6aa1e6c1756f41c92091038bfd7fcd90458
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-17 18:59:50 -0800

    ft_strsplit works for all tests

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/13 16:11:48 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/14 18:44:13 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/17 18:57:23 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include <stdio.h>
 
 /*the purpose of this function is to return the number of words in the input string. this number is useful for allocating an array of pointers such that each pointer within the array points to a string that contains a copy of a word found within the input*/
-ft_word_count(const chat *s, char c)
+static int	ft_word_count(const char *s, char c)
 {
 	int flag;
 	int word;
@@ -36,26 +36,21 @@ ft_word_count(const chat *s, char c)
 	 string.*/
 	while (*s)
 	{
-		/*Each of the following two conditions will execute if the value 
+		/*each of the following two conditions will execute if the value 
 		 being pointed to by s is equal to the value of the delimiter 
-		 character but the vital difference between the behaviors of these 
-		 two conditions made evident as the value of the flag variable changes.
-		 
-		 During the first itteration the value of the flag
+		 character but the vital difference is with the value of the 
+		 flag variable. During the first itteration the value of the flag
 		 variable is 0 so the first condition will not execute regardless
-		 of the value of the pointer. 
-		 
-		 The next condition will execute, 
+		 of the value of the pointer. The next condition will execute, 
 		 if and only if the value of the pointer is equal to the 
-		 value of the delimeter character, meaning the function is not
-		 currently itterating over a word*/
+		 value of the delimeter character.*/
 		if (*s == c && flag == 1)
 		{
-			flag == 0;
+			flag = 0;
 		}
 		/*when the second condition executes the flag variable will be 
-		 * set to 1 and the */
-		if (*s == c && flag == 0)
+		 * set to 1 as an indicator that we are */
+		if (*s != c && flag == 0)
 		{
 			flag = 1;
 			word++;
@@ -64,6 +59,47 @@ ft_word_count(const chat *s, char c)
 	}
 	return (word);
 }
+
+static int	ft_wlen(const char *s, char c)
+{
+	int len;
+
+	len = 0;
+	while (*s != c && *s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+char		**ft_strsplit(char const *s, char c)
+{
+	int		i;
+	int		nb_words;
+	char	**array;
+
+	i = 0;
+	nb_words = ft_word_count((const char *)s, c);
+	array = (char **)malloc(sizeof(*array) * (nb_words + 1));
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	while (nb_words--)
+	{
+		while (*s == c && *s != '\0')
+		{
+			s++;
+		}
+		array[i] = ft_strsub((const char *)s, 0, ft_wlen((const char *)s, c));
+		s = s + ft_wlen((const char *)s, c);
+		i++;
+	}
+	array[i] = NULL;
+	return (array);
+}
+
 /*
 int		*ft_find_str(const char *s, int d)
 {
