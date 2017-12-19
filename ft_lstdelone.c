cf56251f351fb0e1dec17dd7908577194e24a938
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-18 21:03:52 -0800

    ft_lstdelone should work

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/18 15:23:04 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/18 16:57:31 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/18 21:02:49 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,8 @@ void	ft_lstdelone(t_list **alst, void (*del)(void *, size_t))
 {
 	if (*alst)
 	{
-		if ((*alst) -> content || (*alst) -> content_size)
-		{
-			del((*alst) -> content, (*alst) -> content_size);
-		}
-		free (*alst);
-		*alst = NULL;
+		del ((*alst) -> content, (*alst)->content_size);
+		free(*alst);
+		alst = NULL;
 	}
 }
