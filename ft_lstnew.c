c57c78032ad33dac59d49ebba200eb6dffa51fbe
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-20 11:25:12 -0800

    ft_lstnew.c normed

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/18 11:42:23 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/18 15:03:25 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/20 11:24:21 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,32 +14,18 @@
 
 t_list	*ft_lstnew(void const *content, size_t content_size)
 {
-	/*
-	c = content;
-	cs = content_size;
-	t_list	new_link;
-	
-	t_list = NULL;
-	node = ft_memmalloc(sizeof(node));
-	ft_memcpy(link->content, c);
-	new_link->content_size = cs;
-	
-	return (new_link);
-	*/
 	t_list	*elem;
 	void	*cpy;
 
-	/*check for a succesful memory allocation*/	
 	if (!(elem = (t_list *)malloc(sizeof(t_list))))
 	{
 		return (NULL);
 	}
-
-	elem -> next= NULL;
-	if(!content)
+	elem->next = NULL;
+	if (!content)
 	{
-		elem -> content = NULL;
-		elem -> content_size = 0;
+		elem->content = NULL;
+		elem->content_size = 0;
 	}
 	else
 	{
@@ -48,8 +34,8 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 			free(elem);
 			return (NULL);
 		}
-		elem -> content = ft_memcpy(cpy, content, content_size);
-		elem -> content_size = content_size;
+		elem->content = ft_memcpy(cpy, content, content_size);
+		elem->content_size = content_size;
 	}
 	return (elem);
 }
