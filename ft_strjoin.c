e02343ab5c881cce8d885a5a2dea969641b95f53
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-20 14:01:55 -0800

    ft_strjoin normed

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/11 14:58:33 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/11 15:54:55 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/20 14:01:02 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	i;
 	char	*str;
 
-	if((str = ft_strnew(ft_strlen(s1) + ft_strlen(s2) + 1)) && s1 && s2)
+	if ((str = ft_strnew(ft_strlen(s1) + ft_strlen(s2) + 1)) && s1 && s2)
 	{
 		i = 0;
 		while (*s1)
@@ -26,16 +26,14 @@ char	*ft_strjoin(char const *s1, char const *s2)
 			s1++;
 			i++;
 		}
-
 		while (*s2)
 		{
 			*(str + i) = *s2;
 			s2++;
 			i++;
 		}
-
 		*(str + i) = '\0';
 		return (str);
 	}
-	return(NULL);
+	return (NULL);
 }
