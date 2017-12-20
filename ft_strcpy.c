53822665d423e6b91012a8da7ade4b7680e58ec5
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-20 10:41:14 -0800

    ft_strcpy normed

--- ft_strcpy.c
+++ ft_strcpy.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/29 16:35:02 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/02 15:12:10 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/20 10:40:48 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ char	*ft_strcpy(char *dst, const char *src)
 	{
 		dst[i] = src[i];
 	}
-
 	dst[i] = '\0';
 	return (dst);
 }
