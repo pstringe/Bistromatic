06ed9c50e69d562e32bc8cfd4a9959fe6cc8f9e4
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-20 10:38:29 -0800

    ft_puststr_fd normed

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/13 15:35:34 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/13 15:39:04 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/20 10:37:52 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_putstr_fd(char const *s, int fd)
 	int i;
 
 	i = -1;
-	while(s && s[++i])
+	while (s && s[++i])
 	{
 		ft_putchar_fd(*(s + i), fd);
 	}
