fd21c20d3b28ad199ee62d8b0fb2d447186091af
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-20 13:04:13 -0800

    ft_atoi normed

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,13 +6,26 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/17 19:11:53 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/18 09:29:27 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/20 13:01:08 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_atoi(const char *str)
+static int	ft_is_whitespace(int c)
+{
+	if (c == '\n' || c == '\v' || c == '\f')
+	{
+		return (1);
+	}
+	if (c == ' ' || c == '\r' || c == '\t')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int			ft_atoi(const char *str)
 {
 	int i;
 	int nb;
@@ -21,23 +34,17 @@ int		ft_atoi(const char *str)
 	i = 0;
 	nb = 0;
 	flag = 0;
-	while ((str[i] == '\n') || (str[i] == '\v') || (str[i] == '\f') || (str[i] == ' ') || (str[i] == '\r') || (str[i] == '\t'))
-	{
+	while (ft_is_whitespace(str[i]))
 		i++;
-	}
 	if (str[i] == '-')
-	{
 		flag = 1;
-	}
 	if (str[i] == '+' || str[i] == '-')
-	{
 		i++;
-	}
 	while (str[i] && str[i] >= '0' && str[i] <= '9')
 	{
 		nb = nb * 10;
 		nb = nb + str[i] - '0';
-		i ++;
+		i++;
 	}
 	if (flag == 1)
 	{
