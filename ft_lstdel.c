eff91702356271260ba1ef97b308d7737d7654b5
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-20 14:20:50 -0800

    ft_lstdel.c

--- ft_lstdel.c
+++ ft_lstdel.c
@@ -6,13 +6,13 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/18 21:38:49 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/20 09:09:27 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/20 14:20:17 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstdel(t_list **alst, void(*del)(void *, size_t))
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
 {
 	t_list *tmp;
 	t_list *tmp_next;
