081e5b710c34872ee50ca30d0d6956365905c49f
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-20 10:32:40 -0800

    ft_strdup conforms to norme

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/30 11:55:56 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/02 17:52:06 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/20 10:31:53 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 char	*ft_strdup(const char *str)
 {
 	char	*dest;
-	
+
 	if (!(dest = (char*)malloc(ft_strlen(str) + 1)))
 	{
-		return(NULL);
+		return (NULL);
 	}
 	dest = ft_strcpy(dest, str);
-	return(dest);
+	return (dest);
 }
