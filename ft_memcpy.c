d9d5fee2ce632bdee8147fd0a7b859f5d9fdc247
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-20 10:17:52 -0800

    ft_memcpy normed

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 17:08:22 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/05 17:13:27 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/20 10:17:27 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	char 		*s1;
+	char		*s1;
 	const char	*s2;
 
 	s1 = (char *)dst;
