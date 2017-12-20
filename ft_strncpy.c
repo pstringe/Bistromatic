09232fe25bcb13229ca0cd0cf5dffe270acc42fd
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-20 10:21:30 -0800

    ft_strncpy normed

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,9 +6,10 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/29 16:35:02 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/03 18:15:49 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/20 10:20:56 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_strncpy(char *s1, const char *s2, size_t len)
@@ -21,12 +22,11 @@ char	*ft_strncpy(char *s1, const char *s2, size_t len)
 	while (len > 0)
 	{
 		len--;
-		if((*dst++ = *src++) == '\0')
+		if ((*dst++ = *src++) == '\0')
 		{
 			ft_memset(dst, '\0', len);
-			break;
+			break ;
 		}
 	}
 	return (s1);
-		
 }
