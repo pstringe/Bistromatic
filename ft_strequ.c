e2348b7f868389a6841003f184490841bdc964e1
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-20 13:48:39 -0800

    ft_strequ normed

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,13 +6,13 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/04 19:57:59 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/11 13:13:51 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/20 13:47:32 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int 	ft_strequ(const char *s1, const char *s2)
+int		ft_strequ(const char *s1, const char *s2)
 {
 	size_t	i;
 
@@ -20,13 +20,11 @@ int 	ft_strequ(const char *s1, const char *s2)
 	{
 		return (0);
 	}
-
 	i = 0;
-	while(*(s1 + i) && *(s2 + i) && *(s1 + i) == *(s2 + i))
+	while (*(s1 + i) && *(s2 + i) && *(s1 + i) == *(s2 + i))
 	{
 		i++;
 	}
-
 	if ((unsigned char)*(s1 + i) - (unsigned char)*(s2 + i) == 0)
 	{
 		return (1);
