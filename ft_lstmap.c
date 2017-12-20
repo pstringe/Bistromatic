49e18c17faba98ff81eec201846348d7394ba9b3
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-20 10:23:35 -0800

    ft_lstmap normed

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/19 18:46:16 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/20 09:11:51 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/20 10:22:52 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list	*new_list;
 
-	if(lst && f)
+	if (lst && f)
 	{
 		new_list = f(lst);
 		new_list->next = ft_lstmap(lst->next, f);
