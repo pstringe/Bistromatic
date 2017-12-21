294c8d6760185a66fa7323b7e310cc04a200efdb
Poitier Stringer <pstringe@e1z2r12p3.42.us.org>
2017-12-21 11:26:55 -0800

    strnstr.c conforms to style guid and works for all testsw

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/04 18:49:16 by pstringe          #+#    #+#             */
-/*   Updated: 2017/12/11 13:05:57 by pstringe         ###   ########.fr       */
+/*   Updated: 2017/12/21 11:23:05 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,20 @@
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t i;
-	size_t j;
 
-	i = 0;
 	if (*little == '\0')
 	{
 		return ((char *)big);
 	}
-	while (*(big + i))
+	i = ft_strlen(little);
+	while (*big != '\0' && len >= i)
 	{
-		j = 0;
-		while (*(little + j) == *(big + i + j) && *(little + j) && i < len)
+		if (ft_strncmp(big, little, i) == 0)
 		{
-			j++;
+			return ((char *)big);
 		}
-		if (j == ft_strlen(little))
-		{
-			return ((char *)(big + i));
-		}
-		i++;
+		big++;
+		len--;
 	}
 	return (NULL);
 }
