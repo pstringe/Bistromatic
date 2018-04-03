88b9c8030750faf6cc275e184c1b9ceca0f6b02f
Poitier Stringer <pstringe@e1z4r1p8.42.us.org>
2018-04-03 11:56:05 -0700

    mase adjustments, eat lunch, take nap continue.

--- calc.c
+++ calc.c
@@ -6,34 +6,25 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/02 10:04:47 by pstringe          #+#    #+#             */
-/*   Updated: 2018/04/02 21:48:03 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/04/03 11:55:19 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bistromatic.h"
 #include <stdlib.h>
 
-/*
-**	so my goal is to utilize the same method utilized by python to store an arbitratily large
-**	integer. The solution described in the artical is to represent the int as an array of digits
-**	this sounds similar to the method I just attempted with linked lists, but I'm going to attempt
-**	to follow the python code very closely
-*/
-
-/*
-**	acording to the code, the first step is to convert the number from base 10 (or in my case, 
-**	base whatever) to bas 2^30. Each element represents a digit from 0 to 2 ^30. I'll go ahead 
-**	and use use 32 bit integers with 30 bits representing the digits
-*/
-
-typedef struct		*s_bi
+typedef	struct		*s_master
 {
-	int			sign;
-	int			size;
-	t_list		*val;
-}					t_bi;
+	int		base;
+}					t_m;
 
+typedef struct		*s_bn
+{
+	unsigned char	*dig;
+	int				size;
+}					t_bn;
 
+/*
 t_bi	*split_nbr(char *str)
 {
 	s_bi	*nbr;
@@ -60,4 +51,107 @@ t_bi	*split_nbr(char *str)
 	nbr->size = ft_lstlen(nb);
 	nbr->val = val;
 	return (nbr);
+}
+*/
+/*
+
+*/
+
+void	put_bn(t_bn *n)
+{
+	int	i;
+
+	if (n->sig == -1)
+		ft_putchar('-');
+	i = n->lst;
+	while (i-- >= 0)
+		ft_putchar((n->dig)[i] + '0')
+	ft_putchar("\n");
+}
+/*
+void	get_and_print_test(char *str)
+{
+}
+*/
+
+/*
+**	this will substitute the digits within a string for ordered 
+**	values it will work up to base 255, bc supports an ibase of 0 - 16
+**	if for some reason I discover I need to accomodate larger bases, 
+**	I'll just use a larger data-type.
+*/
+
+int		format_sign(char **bnstr)
+{
+	char 	*tmp;
+	int		n;
+
+	n = 1;
+	tmp = *bnstr;
+	if (tmp == '-')
+	{
+		tmp = ft_strdup(++*bnstr);
+		n = -1;
+	}
+	*bnstr = tmp;
+	return (n);
+}
+
+unsigned char	*format_base(char *base, char **bnstr)
+{
+	unsigned char 	*nbase;
+	unsigned char 	i;
+	int				t;
+	unsigned char	*nstr;
+
+	i = -1;
+	t =  1;
+	nstr = *bnstr;
+	nbase = ft_memalloc(sizeof(unsigned char) * 256);
+	while (++i < 256)
+		nbase[i] = ((t = !base[(int)i]) && t) ? i : '\0';
+	i = -1;
+	while(nstr[++i])
+	{
+		j = -1;
+		while (base[++j])
+			nstr[i] = (nstr[i] == base[j]) ? nbase[j] : nstr[i];
+	}
+	return (nbase);
+}
+
+
+void	atobn(char *base, char *bnstr)
+{
+	t_bn	*nbr;
+	int		n;
+	
+	nbr = ft_memalloc(sizeof(t_nbr*));
+	nbr->sig = format_sign(&bnstr);
+	nbr->size = ft_strlen(bnstr);
+	nbr->base = format_base(&bnstr);
+}
+
+/*
+void	base_to_max(char *bnstr, int base, t_bn	*bn)
+{
+	int		i;
+	int		n;
+
+	i = -1;
+	while (bnstr[++i])
+	{
+				
+	}
+}
+*/
+
+void	
+int		main(int argc, char **argv)
+{
+	if(argc == 2)
+	{
+		
+	}
+	return (0);
 }
