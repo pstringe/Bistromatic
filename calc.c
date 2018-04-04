7e5355bc6f08b600d436e0910506eb8e058de261
Poitier Stringer <pstringe@e1z4r1p8.42.us.org>
2018-04-04 10:44:46 -0700

    wrote product and addition functions

--- calc.c
+++ calc.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/02 10:04:47 by pstringe          #+#    #+#             */
-/*   Updated: 2018/04/04 10:10:59 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/04/04 10:44:20 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,11 @@ int		parse_factor();
 **	this global var points to current position inside the string being parsed
 **	for some reason the author of the tutorial I'm going off of felt the need to 
 **	make it global. I will remedy this at a later time.
+**	--------------------------------------------------------------------------
+**	
+**	Now I understand that this variable is global because it is neccessary for each function to
+**	increment the pointer. There are other ways to do this, but this method is actually very
+**	convenient, I believe I could justify it, should I choose to use this method.
 */
 
 char	*g_x;
@@ -43,6 +48,53 @@ int		parse_factor()
 	return (-1);
 }
 
+/*
+**	parses products based on the next value being a multiplicationn operator. If no operator, just returns the number.
+**	this allows us to establish operator precedence by calling the product function from the sum function. That way, if 
+**	there is something to multiply, that will be done first, then addition. Later, instead of just multiplying, W'ell call
+**	our bignum multiplication operator.
+*/
+
+int		parse_product()
+{
+	int f_1;
+	int	f_2;
+
+	f_1 = parse_factor();
+	while (*x == '*')
+	{
+		++x;
+		f_2 = parse_factor();
+		f_1 *= f_2;
+	}
+	return (f_1);
+}
+
+/*
+**	parse sum will calls the parse_product function to get its two addends, garunteeing numbers 
+**	to work with so long as the string is valid. Again, the logic is the same here but we will call a bignu, 
+**	addition function in the final function.
+*/
+
+int		parse_sum()
+{
+	int p_1;
+	int p_2;
+
+	p_1 = parse_product();
+	while (*x == '+')
+	{
+		++x;
+		p_2 = parse_product();
+		p_1 += p_2;
+	}
+	return (p_1);
+}
+
+/*
+**
+**
+*/
 
 int		main()
 {
